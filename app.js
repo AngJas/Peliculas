07c468c9d8ed10d0aee319598b175aa627ee963a
Angel <angeljasiel99@gmail.com>
2026-05-31 19:24:18 -0700

    arreglar sequialice

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+import conexion from './conexion.js';
+
 const express = require("express");
 const { Sequelize, DataTypes } = require("sequelize");
 const jwt = require("jsonwebtoken");
@@ -5,13 +7,6 @@ const bcrypt = require("bcrypt");
 
 const app = express();
 app.use(express.json());
-
-// const SECRET_KEY = process.env.SECRET_KEY;
-
-// if (process.env.NODE_ENV === 'production' && !SECRET_KEY) {
-//     console.error('FATAL: SECRET_KEY must be set in production (process.env.SECRET_KEY)');
-//     process.exit(1);
-// }
 
 app.use((req, res, next) => {
     console.log(`${req.method} ${req.url}`);
@@ -32,27 +27,15 @@ if (process.env.NODE_ENV === 'production' || process.env.DATABASE_SSL === 'true'
     };
 }
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, connectionOptions);
+// const sequelize = new Sequelize(process.env.DATABASE_URL, connectionOptions);
 
-const Pelicula = sequelize.define("Pelicula", {
+const Pelicula = conexion.sequelize.define("Pelicula", {
     titulo: {
         type: DataTypes.STRING,
         allowNull: false
     },
     director: DataTypes.STRING,
     anio: DataTypes.INTEGER
-});
-
-const Usuario = sequelize.define("Usuario", {
-    username: {
-        type: DataTypes.STRING,
-        unique: true,
-        allowNull: false
-    },
-    password: {
-        type: DataTypes.STRING,
-        allowNull: false
-    }
 });
 
 app.get("/peliculas", async (req, res) => {
@@ -67,4 +50,8 @@ const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
     console.log(`Servidor en http://localhost:${PORT}`);
+});
+
+app.get("/", async (req, res) => {
+    res.send("¡Bienvenido a la API de Películas!");
 });
